cf23bc1b728f328bdc5810d909510b3379caab0a
Reyad07 <mreyad30207@gmail.com>
2025-11-19 22:32:11 +0600

    added comments for uvm_tlm_put file

--- src/TLM/uvm_tlm_put.sv
+++ src/TLM/uvm_tlm_put.sv
@@ -5,7 +5,7 @@ class producer extends uvm_component;
     `uvm_component_utils(producer)
 
     int data = 9;
-    uvm_blocking_put_port #(int) send;
+    uvm_blocking_put_port #(int) send;  // port to send data to consumer
 
     function new(string path = "producer", uvm_component parent = null);
         super.new(path, parent);
@@ -14,7 +14,7 @@ class producer extends uvm_component;
 
     virtual task main_phase(uvm_phase phase);
         phase.raise_objection(this);
-        send.put(data);
+        send.put(data); // Call put method on connected consumer and send data
         `uvm_info("PRODUCER", $sformatf("Sent data: %0d", data), UVM_NONE)
         phase.drop_objection(this);
     endtask
@@ -26,16 +26,16 @@ endclass
 class consumer extends uvm_component;
     `uvm_component_utils(consumer)
 
-    uvm_blocking_put_export #(int) recv;
-    uvm_blocking_put_imp #(int, consumer) imp;
+    uvm_blocking_put_export #(int) recv;    // export to receive data from producer
+    uvm_blocking_put_imp #(int, consumer) imp;  // implementation method
 
     function new(string path = "consumer", uvm_component parent = null);
         super.new(path, parent);
-        recv = new("recv", this);
-        imp = new("imp", this);
+        recv = new("recv", this);   //  create export(recv)
+        imp = new("imp", this);   // create implementation(imp)
     endfunction
 
-    task put(int datarecv);
+    task put(int datarecv); // implementation of put method called from producer
         `uvm_info("CONSUMER", $sformatf("Received data: %0d", datarecv), UVM_NONE)
     endtask
 
@@ -55,14 +55,14 @@ class env extends uvm_env;
 
     virtual function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        p = producer::type_id::create("p", this);
+        p = producer::type_id::create("p", this);   
         c = consumer::type_id::create("c", this);
     endfunction
 
     virtual function void connect_phase(uvm_phase phase);
         super.connect_phase(phase);
-        p.send.connect(c.recv);
-        c.recv.connect(c.imp);
+        p.send.connect(c.recv); // connect port(send) to export(recv)  
+        c.recv.connect(c.imp);  // connect export(recv) to implementation(imp)
     endfunction
 
 endclass
